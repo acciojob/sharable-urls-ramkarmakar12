fc25dafa77b8e16c2a656913b9ee12edf322fe35
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-01-05 11:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+// your code here
+function updateURL() {
+            const name = document.getElementById('name').value;
+            const year = document.getElementById('year').value;
+
+            let url = "https://localhost:8080/";
+
+            const params = new URLSearchParams();
+            if (name) params.append("name", name);
+            if (year) params.append("year", year);
+
+            const urlElement = document.getElementById('url');
+            urlElement.textContent = url + (params.toString() ? `?${params.toString()}` : "");
+
+            return false;
+}
